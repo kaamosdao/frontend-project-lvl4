b7343f9b037fa9c7084e27c9e2b2d8d671275908
Alexander <kaamosdao@gmail.com>
2022-04-27 15:28:10 +0300

    remove unused import

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  Button, SplitButton, Nav, Dropdown, ButtonGroup,
+  Button, Nav, Dropdown, ButtonGroup,
 } from 'react-bootstrap';
 import { setCurrentChannel } from '../slices/channelSlice.js';
 import { setModalInfo } from '../slices/modalSlice.js';
